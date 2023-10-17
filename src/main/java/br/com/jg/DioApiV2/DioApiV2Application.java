34e058503785fe858f3776016872ec669ffc032c
Joao Gabriel <joaoglima@hotmail.com>
2023-10-17 17:08:40 -0300

    Atualizando a API para não haver erro de CORS

--- src/main/java/br/com/jg/DioApiV2/DioApiV2Application.java
+++ src/main/java/br/com/jg/DioApiV2/DioApiV2Application.java
@@ -1,9 +1,12 @@
 package br.com.jg.DioApiV2;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server url")})
 public class DioApiV2Application {
 
 	public static void main(String[] args) {
